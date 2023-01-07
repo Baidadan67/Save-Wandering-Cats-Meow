80a177775071bc81c0418c212da8b03bfdd56909
The Cat <56602118+thecatinbox@users.noreply.github.com>
2023-01-07 15:17:15 -0700

    Fix obstacle generator

--- project/Script/Obstacle.gd
+++ project/Script/Obstacle.gd
@@ -1,16 +1,13 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var vSize = 64 
+var hSize = 64 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	position.x = 212 + hSize * (randi() % (600 / hSize))
+	position.y = vSize * (randi() % (600 / vSize))
+		
+	while $Area2D.get_overlapping_areas() or (position.x <= 360 and position.y >= 460): 
+		position.x = 212 + hSize * (randi() % (600 / hSize))
+		position.y = vSize * (randi() % (600 / vSize))
--- project/Script/ObstacleGenerator.gd
+++ project/Script/ObstacleGenerator.gd
@@ -2,21 +2,10 @@ extends Node2D
 
 var maxObstacle = 10
 var obstacleScene = load("res://Scene/Outside/Obstacle.tscn")
-var vSize = 64 
-var hSize = 64 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(maxObstacle): 
 		randomize() 
 		var obstacle = obstacleScene.instance()
-		
-		obstacle.global_position.x = 212 + hSize * (randi() % (600 / hSize))
-		obstacle.global_position.y = vSize * (randi() % (600 / vSize))
-		
-		while obstacle.global_position.x <= 360 and obstacle.global_position.y >= 460: 
-			obstacle.global_position.x = 212 + hSize * (randi() % (600 / hSize))
-			obstacle.global_position.y = vSize * (randi() % (600 / vSize))
-		
-		print(obstacle.global_position.x, ' ', obstacle.global_position.y)
 		add_child(obstacle)
