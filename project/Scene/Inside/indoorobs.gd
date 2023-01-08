60344c6557ddda376bda91f75de5f7ffe2a26073
Baidadan67 <hb2@ualberta.ca>
2023-01-07 22:19:44 -0700

    update rand spawn position indoor

--- project/Scene/Inside/indoorobs.gd
+++ project/Scene/Inside/indoorobs.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var obscats = load("res://Scene/Inside/indoorcats.tscn")
 onready var file = 'res://sprite/catchedcats.txt'
+var screen_size
+var rng = RandomNumberGenerator.new()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +10,11 @@ onready var file = 'res://sprite/catchedcats.txt'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_file(file)
+	screen_size = get_viewport_rect().size
+	load_file(file, screen_size)
+	randomize()
 
-func load_file(file):
+func load_file(file, screen_size):
 
 	var f = File.new()
 	f.open(file, File.READ)
@@ -18,6 +22,9 @@ func load_file(file):
 		var line = f.get_line()
 		print(line)
 		var cat = obscats.instance()
+		cat.position.x = rng.randi_range(0,screen_size.x) 
+		cat.position.y = rng.randi_range(0,screen_size.y)
+		#print(rng.randi_range(0,screen_size.x))
 		add_child(cat)
 	f.close()
 	return
