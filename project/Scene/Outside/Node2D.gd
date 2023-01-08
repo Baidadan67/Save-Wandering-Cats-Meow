ce9d0de200bbc9cea6b98e237cbf828149d0b87d
Xing <57470442+yingyingxing@users.noreply.github.com>
2023-01-07 21:05:26 -0700

    a

--- project/Scene/Outside/Node2D.gd
+++ project/Scene/Outside/Node2D.gd
@@ -4,17 +4,18 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var indoors = load("res://Scene/Inside/InsideRoom.tscn")
+#var indoors = load("res://Scene/Inside/InsideRoom.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#if Input.get_action_strength("ui_select"):
+	#	var indoor = indoors.instance()
+	#	add_child(indoor)
+	pass
 
 
-func _process(delta):
-	if Input.get_action_strength("ui_select"):
-		var indoor = indoors.instance()
-		add_child(indoor)
+#func _process(delta):
+	
 #func _on_ExitOutdoor_input_event(viewport, event, shape_idx):
 	#if (event is InputEventMouseButton && event.pressed):
 	#	# print("Clicked")
