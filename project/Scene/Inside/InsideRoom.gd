fb428ce3ac4d27b7a0e646b9eee757a8b4f5dc73
lily <57424889+ling-rui@users.noreply.github.com>
2023-01-08 01:25:38 -0700

    load

--- project/Scene/Inside/InsideRoom.gd
+++ project/Scene/Inside/InsideRoom.gd
@@ -6,7 +6,5 @@ extends Node2D
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _on_Button_button_up():
+	get_tree().change_scene("res://Scene/Title/TitleRoom.tscn")
