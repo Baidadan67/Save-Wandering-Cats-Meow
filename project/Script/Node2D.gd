498b0ce21167de02870dc3093fd7b75839499370
The Cat <56602118+thecatinbox@users.noreply.github.com>
2023-01-07 23:55:27 -0700

    Fix space to return inside room

--- project/Script/Node2D.gd
+++ project/Script/Node2D.gd
@@ -1,20 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-#var indoors = load("res://Scene/Inside/InsideRoom.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#if Input.get_action_strength("ui_select"):
-	#	var indoor = indoors.instance()
-	#	add_child(indoor)
 	pass
 
-
-#func _process(delta):
+func _process(delta):
+	if Input.is_action_just_pressed("ui_select"):
+		if Input.is_action_pressed("ui_select"): 
+			get_tree().change_scene("res://Scene/Inside/InsideRoom.tscn")
+	
 	
 #func _on_ExitOutdoor_input_event(viewport, event, shape_idx):
 	#if (event is InputEventMouseButton && event.pressed):
--- project/Script/Player.gd
+++ project/Script/Player.gd
@@ -8,13 +8,12 @@ const SIZE = [64, 64]
 var velocity = Vector2.ZERO
 var times = 0
 var pro = [0.3, 0.5, 1]
-var capture = false
-var counter = 50 
-var max_counter = 50 
+var captured = false
+var counter = 100 
+var max_counter = 100 
 var captures = load("res://Scene/Outside/capture.tscn")
 var fails = load("res://Scene/Outside/Fail.tscn")
 var file = File.new()
-
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -51,16 +50,23 @@ func _physics_process(delta):
 						#file.store_line(json)
 						file.close()
 						collision.collider.queue_free()
+						captured = true
 						var capture = captures.instance()
-						add_child(capture)
-						
+						capture.position.x = 850
+						capture.position.y = 508
+						get_parent().add_child(capture)
 					else:
 						print("fail")
 						var fail = fails.instance()
-						add_child(fail)
+						fail.position.x = 850
+						fail.position.y = 508
+						get_parent().add_child(fail)
 					times += 1
-					#print(times)
 					counter = 0
+					#print(times)
 	
-	if counter < max_counter: 
+	if counter < max_counter and captured == false: 
 		counter += 1
+		modulate.a = 0.5
+	else: 
+		modulate.a = 1
