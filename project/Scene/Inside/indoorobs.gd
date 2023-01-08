0bc1db36d66339bef52c893845db8cceaefa3861
Baidadan67 <hb2@ualberta.ca>
2023-01-08 02:13:52 -0700

    1

--- project/Scene/Inside/indoorobs.gd
+++ project/Scene/Inside/indoorobs.gd
@@ -18,15 +18,20 @@ func _ready():
 func load_dat(screen_size):
 	var file = File.new()
 	file.open("res://save_game.dat", File.READ)
-	while not file.eof_reached(): # iterate through all lines until the end of file is reached
+	var counter = 0
+	while (not file.eof_reached() and counter <= 5)  : # iterate through all lines until the end of file is reached
 		var line = file.get_line()
+		
 		#print(line)
 		var cat = obscats.instance()
 		cat.position.x = rng.randi_range(0,screen_size.x) 
 		cat.position.y = rng.randi_range(2*(screen_size.y)/3,screen_size.y)
-		cat.color = line
+		if line != "":
+			cat.color = line
 		#print(rng.randi_range(0,screen_size.x))
-		add_child(cat)
+		
+			add_child(cat)
+			counter += 1
 	file.close()
 	return
 	
